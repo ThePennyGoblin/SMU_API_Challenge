4a066e22e2be7c960ec4f2bbd658e4608d1b4d9f
TheJew <joshstill433@gmail.com>
2024-05-28 12:40:26 -0500

    running the code

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "0633b6c56c74f18f26fbdeab6caa8e31"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "a6b243d463e14b589e6b054364f970a1"
